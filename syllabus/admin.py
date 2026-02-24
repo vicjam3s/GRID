98af4fd41df11a9836266ecc4a51f6bcc1a70fe5
vicjam3s <njiruv.vn@gmail.com>
2026-02-24 14:38:25 +0300

    Refactor admin classes to inherit from ReadOnlyAdmin for consistent read-only behavior; update list displays and ordering for Course, Subject, Topic, and Subtopic models.

--- syllabus/admin.py
+++ syllabus/admin.py
@@ -2,38 +2,43 @@ from django.contrib import admin
 from .models import Course, Subject, Topic, Subtopic
 
 
+class ReadOnlyAdmin(admin.ModelAdmin):
+    """
+    Base admin class to make models fully read-only.
+    """
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ("code", "name", "authority")
+class CourseAdmin(ReadOnlyAdmin):
+    list_display = ("code", "name", "authority", "version")
+    ordering = ("code",)
 
 
 @admin.register(Subject)
-class SubjectAdmin(admin.ModelAdmin):
+class SubjectAdmin(ReadOnlyAdmin):
     list_display = ("name", "course", "order")
+    ordering = ("course", "order")
     list_filter = ("course",)
 
 
 @admin.register(Topic)
-class TopicAdmin(admin.ModelAdmin):
+class TopicAdmin(ReadOnlyAdmin):
     list_display = ("title", "subject", "order")
+    ordering = ("subject", "order")
     list_filter = ("subject__course", "subject")
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "subject" and request.GET.get("course"):
-            kwargs["queryset"] = Subject.objects.filter(
-                course_id=request.GET.get("course")
-            )
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(Subtopic)
-class SubtopicAdmin(admin.ModelAdmin):
+class SubtopicAdmin(ReadOnlyAdmin):
     list_display = ("title", "topic", "order")
+    ordering = ("topic", "order")
     list_filter = ("topic__subject__course", "topic__subject")
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "topic" and request.GET.get("subject"):
-            kwargs["queryset"] = Topic.objects.filter(
-                subject_id=request.GET.get("subject")
-            )
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
